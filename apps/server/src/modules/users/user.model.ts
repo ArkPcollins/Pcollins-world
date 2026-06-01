af7222ce68c4a54a6be29dcd5ea6979cbcd8de76
Promise Johnson <chiemelapromise30@gmail.com>
2026-06-01 17:15:45 +0100

    feature(auth/user): Add the user/auth routes, repository, service, validation, controller authentication/authorization

--- apps/server/src/modules/users/user.model.ts
+++ apps/server/src/modules/users/user.model.ts
@@ -1,53 +1,55 @@
 import mongoose from "mongoose";
 import { UserRole } from "../shared/enums/role.enum";
+import {
+  baseSchemaFields,
+  baseSchemaOptions,
+} from "../shared/database/base.schema";
 
-const userSchema =
-new mongoose.Schema(
-{
-  firstName: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    avatar: String,
+
+    phone: String,
+
+    address: String,
+
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.USER,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    refreshToken: String,
+    ...baseSchemaFields,
   },
-
-  lastName: {
-    type: String,
-    required: true
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-
-  avatar: String,
-
-  phone: String,
-
-  address: String,
-
-  role: {
-    type: String,
-    enum: Object.values(UserRole),
-    default: UserRole.USER
-  },
-
-  isVerified: {
-    type: Boolean,
-    default: false
-  },
-
-  refreshToken: String
-},
-{
-  timestamps: true
-}
+  baseSchemaOptions
 );
 
 export const UserModel = mongoose.model("User", userSchema);
